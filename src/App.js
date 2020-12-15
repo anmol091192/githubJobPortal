2e6d0f5bf71447951301c1f9eb48069564397b18
Anmol Khandekar <anmolkhandekar@Anmols-MacBook-Pro.local>
2020-12-15 02:40:14 -0600

    Custom hook created for retriving Job

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
+import useGetJobs from './useGetJobs';
+import { Container } from '@material-ui/core';
+import Job from './Job';
 
 function App() {
+  const [ params, setParams ] = useState({});
+  const [ page, setPage ] = useState(1);
+  const { jobs, loading, error } = useGetJobs(params, page);
+
   return (
-    <div className="App">
-      Hello World
-    </div>
+    <Container>
+      {loading && <h1>Loading...</h1>}
+      {error && <h1>Error. Try Refreshing</h1>}
+      {jobs.map(job => {
+        return <Job key={job.id} job={job} />
+      })}
+    </Container>
   );
 }
 
