6e6d5cedb4abeca8923dbae00e47725b27f64d5e
Anmol Khandekar <anmolkhandekar@Anmols-MacBook-Pro.local>
2020-12-15 19:25:09 -0600

    simple pagination added

--- src/App.js
+++ src/App.js
@@ -7,18 +7,18 @@ import JobPagination from './JobPagination';
 function App() {
   const [ params, setParams ] = useState({});
   const [ page, setPage ] = useState(1);
-  const { jobs, loading, error } = useGetJobs(params, page);
+  const { jobs, loading, error, hasNextPage } = useGetJobs(params, page);
 
   return (
     <Container className="my-4">
       <h1 className="mb-4">Jobs Listing</h1>
-      <JobPagination page={page} setPage={setPage} /> 
+      <JobPagination page={page} setPage={setPage} hasNextPage={hasNextPage}/> 
       {loading && <h1>Loading...</h1>}
       {error && <h1>Error. Try Refreshing</h1>}
       {jobs.map(job => {
         return <Job key={job.id} job={job} />
       })}
-      <JobPagination page={page} setPage={setPage} /> 
+      <JobPagination page={page} setPage={setPage} hasNextPage={hasNextPage} /> 
     </Container>
   );
 }
